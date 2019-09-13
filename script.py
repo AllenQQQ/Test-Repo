d12866d3171b4938b6690435bf0de1581afcfef5
Allen Qian <binhua_qian@apple.com>
2019-09-13 16:57:00 +0800

    initial commit

---
+++ script.py
@@ -0,0 +1,26 @@
+import math
+import os
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    test = "test"
+    greeting = "Hello {}!".format(who_to_greet)
+    return greeting
+
+
+print(greet("Allen"))
+print(greet("World"))
+r = requests.get("http://coreyms.com")
+print(r.status_code)
+
+
+# name = input("Your name?")
+# print("Hello, ", name)
+
